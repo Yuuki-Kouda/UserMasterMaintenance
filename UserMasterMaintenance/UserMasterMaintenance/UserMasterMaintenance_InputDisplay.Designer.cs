7f5bb9f2a5b34a752d7bad323096ba5e33630465
koda-y <koda-y@evoltech.co.jp>
2019-10-11 13:09:17 +0900

    InputDisplay追加

---
+++ UserMasterMaintenance/UserMasterMaintenance/UserMasterMaintenance_InputDisplay.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace UserMasterMaintenance
+{
+	partial class UserMasterMaintenance_InputDisplay
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(197, 289);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(61, 39);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "OK";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(264, 289);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(61, 39);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "キャンセル";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 49);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(30, 19);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.Text = "ID";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(12, 99);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(30, 19);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.Text = "名前";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(12, 149);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.Size = new System.Drawing.Size(30, 19);
+			this.textBox3.TabIndex = 4;
+			this.textBox3.Text = "年齢";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(12, 199);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(30, 19);
+			this.textBox4.TabIndex = 5;
+			this.textBox4.Text = "性別";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(12, 249);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.ReadOnly = true;
+			this.textBox5.Size = new System.Drawing.Size(30, 19);
+			this.textBox5.TabIndex = 6;
+			this.textBox5.Text = "所属";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(48, 49);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(56, 19);
+			this.textBox6.TabIndex = 7;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(48, 99);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(277, 19);
+			this.textBox7.TabIndex = 8;
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(48, 149);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(56, 19);
+			this.textBox8.TabIndex = 9;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(48, 199);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(47, 16);
+			this.radioButton1.TabIndex = 10;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "男性";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(101, 199);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(47, 16);
+			this.radioButton2.TabIndex = 11;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "女性";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(48, 248);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(277, 20);
+			this.comboBox1.TabIndex = 12;
+			// 
+			// UserMasterMaintenance_InputDisplay
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(337, 340);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "UserMasterMaintenance_InputDisplay";
+			this.Text = "UserMasterMaintenance";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
+}
