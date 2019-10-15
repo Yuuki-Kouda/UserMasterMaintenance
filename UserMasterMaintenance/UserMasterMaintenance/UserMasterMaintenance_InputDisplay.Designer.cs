30dffdb8286af70fb0a64c639d795f3dd4883fed
koda-y <koda-y@evoltech.co.jp>
2019-10-15 18:00:58 +0900

    ボタンイベント追加

--- UserMasterMaintenance/UserMasterMaintenance/UserMasterMaintenance_InputDisplay.Designer.cs
+++ UserMasterMaintenance/UserMasterMaintenance/UserMasterMaintenance_InputDisplay.Designer.cs
@@ -51,15 +51,18 @@
 			this.button1.TabIndex = 0;
 			this.button1.Text = "OK";
 			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// button2
 			// 
+			this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.button2.Location = new System.Drawing.Point(264, 289);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(61, 39);
 			this.button2.TabIndex = 1;
 			this.button2.Text = "キャンセル";
 			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
 			// 
 			// textBox1
 			// 
