59e0b790bbb6b2bbd48f1be0d6191d2df1611634
koda-y <koda-y@evoltech.co.jp>
2019-10-15 18:00:17 +0900

    確認画面表示処理追加

--- UserMasterMaintenance/UserMasterMaintenance/UserMasterMaintenance_InputDisplay.cs
+++ UserMasterMaintenance/UserMasterMaintenance/UserMasterMaintenance_InputDisplay.cs
@@ -21,6 +21,46 @@ namespace UserMasterMaintenance
 			SetScreen(properties);
 		}
 
+		public enum Screens
+		{
+			AddScreen,
+			UpdateScreen,
+			DeleteScreen,
+		}
+
+		/// <summary>
+		/// 画面パラメータ
+		/// </summary>
+		public Screens ScreenParam { get; set; }
+
+		/// <summary>
+		/// OKボタンクリック
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//未入力チェック、同IDチェック
+			if(!ConfirmEntry() || !ConfirmId())
+			{
+				ErrorDisplay errorDisplay = new ErrorDisplay();
+				return;
+			}
+		
+			//確認画面表示処理
+			OutputConfirmationDiialog();
+		}
+
+		/// <summary>
+		/// キャンセルボタンクリック
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button2_Click(object sender, EventArgs e)
+		{
+			//このフォームを閉じたい
+		}
+
 		/// <summary>
 		/// 出力画面設定
 		/// </summary>
@@ -30,11 +70,12 @@ namespace UserMasterMaintenance
 			{
 				case PropertiesClass.Buttons.AddButton:
 					//追加画面作成
-
+					ScreenParam = Screens.AddScreen;
 					break;
 
 				case PropertiesClass.Buttons.UpdateButton:
 					//更新画面作成
+					ScreenParam = Screens.UpdateScreen;
 					textBox6.Text = properties.DataId;
 					textBox7.Text = properties.DataName;
 					textBox8.Text = properties.DataAge;
@@ -54,6 +95,7 @@ namespace UserMasterMaintenance
 
 				case PropertiesClass.Buttons.DeleteButton:
 					//削除画面設定
+					ScreenParam = Screens.DeleteScreen;
 					textBox6.Text = properties.DataId;
 					textBox7.Text = properties.DataName;
 					textBox8.Text = properties.DataAge;
@@ -79,6 +121,56 @@ namespace UserMasterMaintenance
 					break;
 			}
 			return;
+		}
+
+		/// <summary>
+		/// 確認画面表示処理
+		/// </summary>
+		public void OutputConfirmationDiialog()
+		{
+			switch (ScreenParam)
+			{
+				case Screens.AddScreen:
+					//何もしない
+					break;
+
+				case Screens.UpdateScreen:
+					//確認ポップへ
+					break;
+
+				case Screens.DeleteScreen:
+					//確認ポップへ
+					break;
+			}
+		}
+
+		/// <summary>
+		/// 未入力チェック
+		/// </summary>
+		/// <returns></returns>
+		public bool ConfirmEntry()
+		{
+			if(string.IsNullOrEmpty(textBox6.Text) || 
+				string.IsNullOrEmpty(textBox7.Text) || 
+				string.IsNullOrEmpty(textBox8.Text) || 
+				string.IsNullOrEmpty(comboBox1.Text))
+			{
+				return false;
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// 同IDチェック
+		/// </summary>
+		/// <returns></returns>
+		public bool ConfirmId()
+		{
+			//IDがjsonにあったら
+			return false;
+
+			//IDがなかったら
+			return true;
 		}
 	}
 }
