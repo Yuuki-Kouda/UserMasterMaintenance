2910168490c17bb6d353072079d07b5ade8a166e
koda-y <koda-y@evoltech.co.jp>
2019-10-24 17:18:17 +0900

    Usersクラス破棄

--- UserMasterMaintenance/UserMasterMaintenance/UserMasterMaintenance_ListDisplay.cs
+++ UserMasterMaintenance/UserMasterMaintenance/UserMasterMaintenance_ListDisplay.cs
@@ -41,47 +41,8 @@ namespace UserMasterMaintenance
 		}
 
 		/// <summary>
-		/// Usersクラス（users.json）
 		/// エラータイプパラメータ
 		/// </summary>
-		public class Users
-		{
-			/// <summary>
-			/// ID
-			/// </summary>
-			public string UserId { get; set; }
-
-			/// <summary>
-			/// 名前
-			/// </summary>
-			public string UserName { get; set; }
-
-			/// <summary>
-			/// 年齢
-			/// </summary>
-			public string UserAge { get; set; }
-
-			/// <summary>
-			/// 性別
-			/// </summary>
-			public string UserGender { get; set; }
-
-			/// <summary>
-			/// 所属
-			/// </summary>
-			public string UserAffiliation { get; set; }
-		}
-
-		/// <summary>
-		/// Departmentsクラス（departments.json）
-		/// </summary>
-		public class Departments
-		{
-			/// <summary>
-			/// 所属
-			/// </summary>
-			public string Department { get; set; }
-		}
 		public ErrorType ErrorTypeParam { get; set; }
 
 		/// <summary>
